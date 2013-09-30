b81aa666eda62978c9fbadd783d37d56e6bad8f4
jakenotjacob <jake.campbell91@gmail.com>
2013-09-30 08:52:51 -0600

    Intro to testing with MiniTest.

---
+++ example/example_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+
+class ExampleTest < MiniTest::Unit::TestCase
+  #Methods must be preceded with "test_" to fully inherit MiniTest functions
+  def test_truth
+    assert true
+  end
+end

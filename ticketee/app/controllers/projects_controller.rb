0d4b2192835939352890a3d577880675ccab52f1
jakenotjacob <jake.campbell91@gmail.com>
2013-09-30 19:03:52 -0600

    Projects Controller #Save action created, Projects View created.

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -6,4 +6,15 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def create
+    @project = Project.new( params[:object] )
+
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      # Nadar en la piscina
+    end
+  end
+
 end

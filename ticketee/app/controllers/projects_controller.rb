8723cbd5e8f8569818475d95547873fa4daa7439
jakenotjacob <jake.campbell91@gmail.com>
2013-10-07 15:45:00 -0600

    Added Strong Params to Projects#create.  Added ignore-all-temporary to git config.

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new( params[:project] )
+    @project = Project.new( project_params )
 
     if @project.save
       flash[:notice] = "Project has been created."
@@ -18,6 +18,7 @@ class ProjectsController < ApplicationController
   end
 
   private
+
     def project_params
       params.require(:project).permit(:name, :description)
     end

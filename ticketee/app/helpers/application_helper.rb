dae4fde7d8b2ce5d47eb2940962b600bf34fd93f
jakenotjacob <jake.campbell91@gmail.com>
2013-10-09 15:25:58 -0600

    Added title functionality for #show page

--- ticketee/app/helpers/application_helper.rb
+++ ticketee/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      #Define a named block of content "title"
+      content_for :title do
+        (parts << "Ticketee").join(" - ")
+      end
+    end
+  end
 end

06a1a059cbd3c96af24c32b23008bef50c50f5b1
jakenotjacob <jake.campbell91@gmail.com>
2013-10-28 15:21:40 -0600

    Ticket Validations complete; Creating Tickets for a Project complete.

--- ticketee/spec/features/creating_projects_spec.rb
+++ ticketee/spec/features/creating_projects_spec.rb
@@ -26,6 +26,6 @@ feature 'Creating Projects' do
     click_button 'Create Project'
 
     expect(page).to have_content("Project has not been created.")
-    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Name can't be blank.")
   end
 end

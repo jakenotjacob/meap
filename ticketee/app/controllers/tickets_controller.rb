e5c0d6005be48ce3ad70e1f9d182a3e56b155d17
jakenotjacob <jake.campbell91@gmail.com>
2013-10-28 12:09:23 -0600

    Creating Tickets functionality added.

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -5,8 +5,23 @@ class TicketsController < ApplicationController
     @ticket = @project.tickets.build
   end
 
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been created."
+      render action: "new"
+    end
+  end
+
   private
   def set_project
     @project = Project.find(params[:project_id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description)
   end
 end

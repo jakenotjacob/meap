cbc7448e754e51fb8c1eaf0ee356edb701814121
jakenotjacob <jake.campbell91@gmail.com>
2013-11-05 05:38:14 -0700

    Sessions ROUTES fixed.

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,4 +1,6 @@
 Ticketee::Application.routes.draw do
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
   root to: "projects#index"
   resources :projects do
     resources :tickets

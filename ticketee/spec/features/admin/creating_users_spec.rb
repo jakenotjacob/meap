b848d99a95608ca198be1719d3f8272c489babb4
jakenotjacob <jake.campbell91@gmail.com>
2013-11-14 09:15:09 -0700

    Added Admin form partial for user creation; added Admin/Users#create action

--- ticketee/spec/features/admin/creating_users_spec.rb
+++ ticketee/spec/features/admin/creating_users_spec.rb
@@ -14,7 +14,7 @@ feature "Creating Users" do
   scenario "Creating a new user" do
     fill_in "Email", with: "newbie@example.com"
     fill_in "Password", with: "password"
-    click_button "Create user"
+    click_button "Create User"
     expect(page).to have_content("User has been created.")
   end
 end

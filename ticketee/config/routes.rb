bd7736021e39b50781e0a1855079e3eb40d5e94c
jakenotjacob <jake.campbell91@gmail.com>
2013-11-13 21:43:43 -0700

    Corrected routes file with correct Namespace and Resource

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,10 +1,16 @@
 Ticketee::Application.routes.draw do
-  get "users/index"
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
   root to: "projects#index"
+
   resources :projects do
     resources :tickets
   end
+
   resources :users
+
+  namespace :admin do
+    resources :users
+  end
+
 end

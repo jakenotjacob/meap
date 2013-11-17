426f1ff599f511812d3571d145c624d29dbd915f
jakenotjacob <jake.campbell91@gmail.com>
2013-11-17 15:09:11 -0700

    Added scoping method, modified Project model with correct associations.

--- ticketee/app/models/project.rb
+++ ticketee/app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ActiveRecord::Base
   validates :name, presence: true
+  #Links projects to 'thing' association on Permission objects
+  #used in scope 'viewable_by' below
+  has_many :permissions, as: :thing
+
+  scope :viewable_by, ->(user) do
+    joins(:permissions).where(permissions: { action: "view", user_id: user.id })
+  end
   has_many :tickets, dependent: :delete_all
 end
--- ticketee/spec/controllers/projects_controller_spec.rb
+++ ticketee/spec/controllers/projects_controller_spec.rb
@@ -8,6 +8,7 @@ describe ProjectsController do
       sign_in(user)
     end
 
+    #Action testing: commented for clarity
     {
       new: :get,
       create: :post,
@@ -23,6 +24,15 @@ describe ProjectsController do
         expect(response).to redirect_to(root_path)
         expect(flash[:alert]).to eql("You must be an admin to do that.")
       end
+    end
+
+    it "cannot access the show action without permission" do
+      project = FactoryGirl.create(:project)
+      get :show, id: project.id
+
+      expect(response).to redirect_to(projects_path)
+      expect(flash[:alert]).to eql("The project you were looking " +
+                                  "for could not be found.")
     end
 
     it "displays an error for a missing project" do

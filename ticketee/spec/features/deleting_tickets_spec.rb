0f62b52e3e526674f4dbeb23dc93a5c40b69f5f6
jakenotjacob <jake.campbell91@gmail.com>
2013-11-22 09:36:56 -0700

    Restrict destroy action to only people with permission.

--- ticketee/spec/features/deleting_tickets_spec.rb
+++ ticketee/spec/features/deleting_tickets_spec.rb
@@ -11,6 +11,7 @@ feature 'Deleting tickets' do
 
   before do
     define_permission!(user, "view", project)
+    define_permission!(user, "delete tickets", project)
     sign_in_as!(user)
 
     visit '/'
@@ -24,4 +25,6 @@ feature 'Deleting tickets' do
     expect(page).to have_content("Ticket has been deleted.")
     expect(page.current_url).to eq(project_url(project))
   end
+
 end
+

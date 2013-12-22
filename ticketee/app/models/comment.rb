57a6ac12f1447043da518aa81c54231535a824a0
jakenotjacob <jake.campbell91@gmail.com>
2013-12-21 21:30:48 -0700

    Users can add Tags when adding a Comment.

--- ticketee/app/models/comment.rb
+++ ticketee/app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   before_create :set_previous_state
-  after_create :set_ticket_state
+  after_create :set_ticket_state, :associate_tags_with_ticket
 
   belongs_to :ticket
   belongs_to :user
@@ -12,6 +12,14 @@ class Comment < ActiveRecord::Base
   #Setup ticket->project association
   delegate :project, to: :ticket
 
+  def tag_names
+    @tag_names
+  end
+
+  def tag_names=(names)
+    @tag_names = names
+  end
+
   private
     def set_ticket_state
       self.ticket.state = self.state
@@ -20,6 +28,16 @@ class Comment < ActiveRecord::Base
 
     def set_previous_state
       self.previous_state = ticket.state
+    end
+
+    def associate_tags_with_ticket
+      if tag_names
+        tags = tag_names.split(" ").map do |name|
+          Tag.find_or_create_by(name: name)
+        end
+        self.ticket.tags += tags
+        self.ticket.save
+      end
     end
 end
 
